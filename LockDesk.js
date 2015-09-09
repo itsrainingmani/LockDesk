a0ec8e8b1739d6cbb0cdf3ea82ce44ddb21e57a9
Manikandan <mtiruman@buffalo.edu>
2015-09-09 16:38:26 -0400

    Adding database support

--- LockDesk.js
+++ LockDesk.js
@@ -1,17 +1,10 @@
+Desks = new Mongo.Collection("desks");
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    desks: function () {
+      return Desks.find({});
     }
   });
 }
